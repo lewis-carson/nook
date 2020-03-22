342f712226eb926c7ecd4e9883b3099d2e16509e
eti <eti@eti.tf>
2020-03-22 15:36:54 +0100

    main.go -> nook.go

---
+++ nook.go
@@ -0,0 +1,97 @@
+package main
+
+// importations
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
+
+	"github.com/zserge/webview"
+)
+
+// variables
+var wv = webview.New(true)
+var title = "nook"
+var windowHeight = 400
+var windowWidth = 800
+
+//// functions
+// print the content of a file -- useful for "importation"
+func printFile(file string) (response string) {
+	content, err := ioutil.ReadFile(file)
+	if err != nil {
+		fmt.Println("error reading file:", err)
+		return
+	}
+	data := (string(content))
+	return (data)
+}
+
+// returns the path of the current directory
+func currentDir() (response string) {
+	dir, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+	}
+	return (dir)
+}
+
+func view() {
+	defer wv.Destroy()
+	wv.SetTitle(title)
+	wv.SetSize(windowWidth, windowHeight, webview.HintFixed)
+	wv.Navigate("file://" + currentDir() + "/view/index.html")
+	wv.Run()
+}
+
+func irc() {
+	time.Sleep(time.Second * 1) // sleep for 1 second otherwise messages won't load -- needs work
+
+	user := "tom nook"
+	message := "sent from golang!"
+	newMessage(user, message)
+}
+
+func newMessage(user string, message string) {
+	js := `
+	log = document.getElementById("log");
+	line = document.createElement("li");
+	userName = document.createElement("a");
+	userNameSpan = document.createElement("span");
+	userMessageSpan = document.createElement("span");
+
+	userText = document.createTextNode("` + user + `");
+	userMessage = document.createTextNode(" ` + message + ` ");
+
+	log.appendChild(line);
+	line.appendChild(userNameSpan).classList.add("name", "msg")
+
+	userNameSpan.appendChild(userName);
+	userName.appendChild(userText);
+
+	line.appendChild(userMessageSpan).classList.add("message");
+	userMessageSpan.appendChild(userMessage);`
+
+	wv.Dispatch(func() {
+		fmt.Println("trying to inject msg...")
+		wv.Eval(js)
+	})
+}
+
+func sendMessage() {
+	err := wv.Bind("sendMessage", func() string {
+		// fmt.Println()
+		return "<message>"
+	})
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+// execution
+func main() {
+	go irc()
+	view()
+}
--- view/js/main.js
+++ view/js/main.js
@@ -1,3 +1,4 @@
+// scroll to the bottom of the buffer
 function scrollToBottom(id){
    var element = document.getElementById(id);
    element.scrollTop = element.scrollHeight;
@@ -6,3 +7,28 @@ function scrollToBottom(id){
 document.addEventListener('DOMContentLoaded', function() {
     scrollToBottom("buffer");
 });
+
+
+// add a message to the buffer
+// function addMessage() {
+//     log = document.getElementById("log");
+//     line = document.createElement("li");
+//     userName = document.createElement("a");
+//     userNameSpan = document.createElement("span");
+//     userMessageSpan = document.createElement("span");
+
+//     userText = document.createTextNode("eti");
+//     userMessage = document.createTextNode("heyyyyyyy");
+
+//     log.appendChild(line);
+//     line.appendChild(userNameSpan).classList.add("name", "msg")
+
+//     userNameSpan.appendChild(userName);
+//     userName.appendChild(userText);
+
+//     line.appendChild(userMessageSpan).classList.add("message");
+//     userMessageSpan.appendChild(userMessage);
+
+//     scrollToBottom("buffer");
+// }
+
